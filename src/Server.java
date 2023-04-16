b2a12d0fbb0c52ff53d935b10517b40a3a18371e
Albionii <albii.java22@gmail.com>
2023-04-16 15:10:01 +0200

    Krijimi i dy klasave

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,13 @@
+import java.net.ServerSocket;
+
 public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+
+    }
 }
